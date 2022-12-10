990f1505592c8c3d7a656009e37ff2700f98c50a
Sergey Vodwood <sergey.vodwood@gmail.com>
2022-12-11 00:20:45 +0200

    add ACL's, shorten hc interval on the alb

--- vpc.py
+++ vpc.py
@@ -1,6 +1,6 @@
 import pulumi
 from pulumi_aws import ec2, config, get_availability_zones
-from settings import general_tags, demo_vpc_cidr, demo_private_subnet_cidrs, demo_public_subnet_cidrs
+from settings import general_tags, demo_vpc_cidr, demo_private_subnet_cidrs, demo_public_subnet_cidrs, nginx_stub_status_port
 
 """
 PR.PT-3 "The principle of least functionality is incorporated by configuring systems to provide only essential capabilities"
@@ -31,6 +31,171 @@ demo_igw = ec2.InternetGateway("demo-igw",
 )
 
 """
+Network Access Control Lists
+"""
+# Creates public subnet ACL:
+public_acl = ec2.NetworkAcl("demo-public-acl",
+    vpc_id=demo_vpc.id,
+    tags={**general_tags, "Name": f"demo-public-acl"},
+    opts=pulumi.ResourceOptions(parent=demo_vpc)
+)
+
+# Creates inbound public subnet ACL rules:
+public_network_acl_rule_inbound_100 = ec2.NetworkAclRule("public-nacl-inbound-100",
+    network_acl_id=public_acl.id,
+    rule_number=100,
+    egress=False,
+    protocol="tcp",
+    rule_action="allow",
+    cidr_block="0.0.0.0/0",
+    from_port=80,
+    to_port=80,
+    opts=pulumi.ResourceOptions(parent=public_acl)
+)
+
+public_network_acl_rule_inbound_110 = ec2.NetworkAclRule("public-nacl-inbound-110",
+    network_acl_id=public_acl.id,
+    rule_number=110,
+    egress=False,
+    protocol="tcp",
+    rule_action="allow",
+    cidr_block="0.0.0.0/0",
+    from_port=1024,
+    to_port=65535,
+    opts=pulumi.ResourceOptions(parent=public_acl)
+)
+
+# Creates outbound public subnet ACL rules:
+public_network_acl_rule_outbound_100 = ec2.NetworkAclRule("public-nacl-outbound-100",
+    network_acl_id=public_acl.id,
+    rule_number=100,
+    egress=True,
+    protocol="tcp",
+    rule_action="allow",
+    cidr_block="0.0.0.0/0",
+    from_port=80,
+    to_port=80,
+    opts=pulumi.ResourceOptions(parent=public_acl)
+)
+
+public_network_acl_rule_outbound_110 = ec2.NetworkAclRule("public-nacl-outbound-110",
+    network_acl_id=public_acl.id,
+    rule_number=110,
+    egress=True,
+    protocol="tcp",
+    rule_action="allow",
+    cidr_block="0.0.0.0/0",
+    from_port=443,
+    to_port=443,
+    opts=pulumi.ResourceOptions(parent=public_acl)
+)
+
+public_network_acl_rule_outbound_120 = ec2.NetworkAclRule("public-nacl-outbound-120",
+    network_acl_id=public_acl.id,
+    rule_number=120,
+    egress=True,
+    protocol="tcp",
+    rule_action="allow",
+    cidr_block="0.0.0.0/0",
+    from_port=1024,
+    to_port=65535,
+    opts=pulumi.ResourceOptions(parent=public_acl)
+)
+
+# Creates private subnet ACL:
+private_acl = ec2.NetworkAcl("demo-private-acl",
+    vpc_id=demo_vpc.id,
+    tags={**general_tags, "Name": f"demo-private-acl"},
+    opts=pulumi.ResourceOptions(parent=demo_vpc)
+)
+
+# Create inbound private subnet ACL rules:
+private_network_acl_rule_inbound_100 = ec2.NetworkAclRule("private-nacl-inbound-100",
+    network_acl_id=private_acl.id,
+    rule_number=100,
+    egress=False,
+    protocol="tcp",
+    rule_action="allow",
+    cidr_block=demo_vpc_cidr,
+    from_port=80,
+    to_port=80,
+    opts=pulumi.ResourceOptions(parent=private_acl)
+)
+
+private_network_acl_rule_inbound_110 = ec2.NetworkAclRule("private-nacl-inbound-110",
+    network_acl_id=private_acl.id,
+    rule_number=110,
+    egress=False,
+    protocol="tcp",
+    rule_action="allow",
+    cidr_block=demo_vpc_cidr,
+    from_port=443,
+    to_port=443,
+    opts=pulumi.ResourceOptions(parent=private_acl)
+)
+
+private_network_acl_rule_inbound_120 = ec2.NetworkAclRule("private-nacl-inbound-120",
+    network_acl_id=private_acl.id,
+    rule_number=120,
+    egress=False,
+    protocol="tcp",
+    rule_action="allow",
+    cidr_block=demo_vpc_cidr,
+    from_port=int(nginx_stub_status_port),
+    to_port=int(nginx_stub_status_port),
+    opts=pulumi.ResourceOptions(parent=private_acl)
+)
+
+private_network_acl_rule_inbound_130 = ec2.NetworkAclRule("private-nacl-inbound-130",
+    network_acl_id=private_acl.id,
+    rule_number=130,
+    egress=False,
+    protocol="tcp",
+    rule_action="allow",
+    cidr_block="0.0.0.0/0",
+    from_port=1024,
+    to_port=65535,
+    opts=pulumi.ResourceOptions(parent=private_acl)
+)
+
+# Create outbound private subnet ACL rules:
+private_network_acl_rule_outbound_100 = ec2.NetworkAclRule("private-nacl-outbound-100",
+    network_acl_id=private_acl.id,
+    rule_number=100,
+    egress=True,
+    protocol="tcp",
+    rule_action="allow",
+    cidr_block="0.0.0.0/0",
+    from_port=80,
+    to_port=80,
+    opts=pulumi.ResourceOptions(parent=private_acl)
+)
+
+private_network_acl_rule_outbound_110 = ec2.NetworkAclRule("private-nacl-outbound-110",
+    network_acl_id=private_acl.id,
+    rule_number=110,
+    egress=True,
+    protocol="tcp",
+    rule_action="allow",
+    cidr_block="0.0.0.0/0",
+    from_port=443,
+    to_port=443,
+    opts=pulumi.ResourceOptions(parent=private_acl)
+)
+
+private_network_acl_rule_outbound_120 = ec2.NetworkAclRule("private-nacl-outbound-120",
+    network_acl_id=private_acl.id,
+    rule_number=120,
+    egress=True,
+    protocol="tcp",
+    rule_action="allow",
+    cidr_block=demo_vpc_cidr,
+    from_port=1024,
+    to_port=65535,
+    opts=pulumi.ResourceOptions(parent=private_acl)
+)
+
+"""
 Demo Subnet Topology: two public subnets and two private subnets
 """
 # Create subnets:
@@ -47,6 +212,12 @@ for i in range(2):
         availability_zone=demo_azs[i],
         tags={**general_tags, "Name": f"demo-public-subnet-{prefix}"},
         opts=pulumi.ResourceOptions(parent=demo_vpc)
+    )
+    
+    demo_public_nacl_association = ec2.NetworkAclAssociation(f"public-nacl-association-{prefix}",
+        network_acl_id=public_acl.id,
+        subnet_id=demo_public_subnet.id,
+        opts=pulumi.ResourceOptions(parent=demo_public_subnet)
     )
     
     demo_public_subnets.append(demo_public_subnet)
@@ -88,6 +259,12 @@ for i in range(2):
         availability_zone=demo_azs[i],
         tags={**general_tags, "Name": f"demo-private-subnet-{prefix}"},
         opts=pulumi.ResourceOptions(parent=demo_vpc)
+    )
+    
+    demo_private_nacl_association = ec2.NetworkAclAssociation(f"private-nacl-association-{prefix}",
+        network_acl_id=private_acl.id,
+        subnet_id=demo_private_subnet.id,
+        opts=pulumi.ResourceOptions(parent=demo_private_subnet)
     )
     
     demo_private_subnets.append(demo_private_subnet)
